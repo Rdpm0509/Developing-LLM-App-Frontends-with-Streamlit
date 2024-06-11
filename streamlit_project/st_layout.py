5dc80d94ad36ffb9ef96fb0a17098f6bd1f73c6d
Rdpm0509 <rdpm0509@gmail.com>
2024-06-11 07:31:50 +0200

    adding columns to the layout

--- streamlit_project/st_layout.py
+++ streamlit_project/st_layout.py
@@ -6,3 +6,23 @@ my_select_box = st.sidebar.selectbox('Select', ['US', 'UK', 'DE', 'FR'])
 
 #side bar with a slider
 my_slider = st.sidebar.slider('Temperature')
+
+left_column, right_column = st.columns(2) # it returns 2 columns
+
+import random
+data = [random.random() for _ in range(100)]
+
+with left_column: 
+    st.subheader('A linhechart')
+    st.line_chart(data)
+
+right_column.subheader('Data')
+right_column.write(data[:10])
+
+col1, col2, col3 = st.columns([0.2, 0.5, 0.3]) #up to 20%, 50%, 30%
+col1.markdown('Hello Streamlit World!')
+col2.write(data[5:10])
+
+with col3: 
+    st.header('A cat')
+    st.image('https://static.streamlit.io/examples/cat.jpg')
